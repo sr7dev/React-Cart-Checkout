49a36abab1ef08ff637cb15d6d58852cf93e59d0
white <whitecalm@outlook.com>
2021-01-13 12:55:10 +0300

    init project

---
+++ src/components/Recipe.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import StripeCheckout from 'react-stripe-checkout';
+
+//import { addShipping } from './actions/cartActions'
+class Recipe extends Component{
+    
+    componentWillUnmount() {
+         if(this.refs.shipping.checked)
+              this.props.substractShipping()
+    }
+
+    handleChecked = (e)=>{
+        if(e.target.checked){
+            this.props.addShipping();
+        }
+        else{
+            this.props.substractShipping();
+        }
+    }
+
+    onToken = (token) => {
+        fetch('/save-stripe-token', {
+            method: 'POST',
+            body: JSON.stringify(token),
+        }).then(response => {
+            response.json().then(data => {
+            alert(`We are in business, ${data.email}`);
+            });
+        });
+    }
+    
+    render(){
+  
+        return(
+            <div className="container">
+                <div className="collection">
+                    <li className="collection-item">
+                            <label>
+                                <input type="checkbox" ref="shipping" onChange= {this.handleChecked} />
+                                <span>Shipping(+6$)</span>
+                            </label>
+                        </li>
+                        <li className="collection-item"><b>Total: {this.props.total} $</b></li>
+                    </div>
+                    <div className="checkout">
+                    <StripeCheckout
+                        token={this.onToken}
+                        stripeKey="pk_test_TYooMQauvdEDq54NiTphI7jx"
+                    />
+                    </div>
+                 </div>
+        )
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return{
+        addedItems: state.addedItems,
+        total: state.total
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        addShipping: ()=>{dispatch({type: 'ADD_SHIPPING'})},
+        substractShipping: ()=>{dispatch({type: 'SUB_SHIPPING'})}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Recipe)
